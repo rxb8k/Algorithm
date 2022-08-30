ed5f1cd539c781de493864c40f686fc2316ce8e9
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-08-30 16:17:23 +0900

    fix gitignore

--- Not Solved Yet/1008_A나누기B.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    double A, B;
-    cin>>A>>B;
-    cout<<A/B;
-}
--- Not Solved Yet/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-    // while(1){
-    //     int n=num%10;
-    //     count[num]++;
-    //     int num=num/10;
-    //     if(num<0) break;
-    // }
