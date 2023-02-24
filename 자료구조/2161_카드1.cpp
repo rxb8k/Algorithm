f9582e72162e080dd19b8008a10712986811cc62
rxb8k <jssong.dev@gmail.com>
2023-02-24 21:32:03 +0900

    [Silver V] 큐의 FIFO 특성을 활용한 구현

---
+++ 자료구조/2161_카드1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main () {
+    int N;
+    queue<int> q;
+    
+    cin>>N;
+
+    for (int i=1; i<=N; i++) {
+        q.push(i); // 1~N FIFO : 1, 2, 3, ... , N
+    }
+
+    while (q.size() > 1) {
+        cout<<q.front()<<' '; // 버리는 카드 출력
+        q.pop();
+        int top = q.front();
+        q.pop();
+        q.push(top);
+        if (q.size() == 1) break;
+    }
+
+    cout<<q.front();
+}
