2e879279d95a5abe7a737b285fa3b307dd6dcf18
rxb8k <jssong.dev@gmail.com>
2023-08-06 23:47:11 +0900

    [Level 1] += 연산자를 사용한 문자열 더하기

---
+++ 문자열/가운데_글자_가져오기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int len = s.size();
+    if (len%2==1) {
+        answer = s[len/2];
+    }
+    else {
+        answer += s[len/2-1];
+        answer += s[len/2];
+    }
+    return answer;
+}
